592b4c1c92541ae842a84c6092ccb6751c2c8724
romazakharchenko <89213748002trtrr@gmail.com>
2023-09-20 13:34:32 +0300

    Исправлена ошибка

--- OPD/lab0.0/script3.sh
+++ OPD/lab0.0/script3.sh
@@ -7,7 +7,7 @@ chmod u-r lab0/bronzor4/archeops
 
 #создать симвоическую ссылку для файла magcarg6 с именем lab0/nidoranF4/graveleermagcargo
 chmod u+w lab0/nidoranF4
-ln -s ../magcargo6 lab0/nidoranF4/gravelermagcargo
+ln -s /home/studs/s408648/OPD/lab1/lab0/magcargo6 lab0/nidoranF4/gravelermagcargo
 chmod u-w lab0/nidoranF4
 
 #скопировать файл magcargo6 в директорию lab0/magby8/persian
@@ -25,7 +25,14 @@ chmod u-r lab0/clefable0
 #скопировать рекурсивно дирректорию nidoranF4 в директорию lab0/nidoranF4/clefairy
 cd lab0
 mkdir tmp
-cp -rT nidoranF4 tmp/
+#cp -r nidoranF4/* tmp/
+cp -r nidoranF4/clefable tmp/
+cp -r nidoranF4/clefairy tmp/
+cp -r nidoranF4/graveler tmp/
+cp -r nidoranF4/kabutops tmp/
+cp -r nidoranF4/vibrava tmp/
+cp -r nidoranF4/zigzagoon tmp/
+cp -P nidoranF4/gravelermagcargo tmp
 chmod u+w nidoranF4/clefairy
 mv tmp nidoranF4/clefairy/
 mv nidoranF4/clefairy/tmp nidoranF4/clefairy/nidoranF4
@@ -38,7 +45,8 @@ ln -s bronzor4 lab0/Copy_63
 #скопировать содержимое файла seaking8 в новый файл lab0/nidoranF4/zigzagoonseaking
 chmod u+r lab0/seaking8
 chmod u+w lab0/nidoranF4
-cp lab0/seaking8 lab0//zigzagoonseaking
+touch lab0/nidoranF4/zigzagoonseaking
+cp lab0/seaking8 lab0/nidoranF4/zigzagoonseaking
 chmod u+w lab0/nidoranF4
 chmod u-r lab0/seaking8
 
